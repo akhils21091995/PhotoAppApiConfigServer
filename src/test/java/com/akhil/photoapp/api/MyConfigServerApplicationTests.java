9c35316a10a217ab0b10eef36b802a73564d6aef
akhils21091995 <akhils21091995@gmail.com>
2023-01-29 20:42:59 +0530

    initial commit

---
+++ src/test/java/com/akhil/photoapp/api/MyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.photoapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
